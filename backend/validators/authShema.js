806f38e1677be05c31eced3bb53f618739f7ff88
suryadev78 <pandeysuryadev00@gmail.com>
2025-09-04 19:56:15 +0530

    added authShema to validate signup,login body

---
+++ backend/validators/authShema.js
@@ -0,0 +1,13 @@
+import z, { email } from "zod";
+
+export const signUpShema = z.object({
+    Name : z.string().min(5,"Name must be atleast 5 characters").max(60,"Name must be atmost 60 characters"),
+    Email :email().min(5,"email must be atleast 5 characters").max(30,"email cannot be more than 30 characters"),
+    Address: z.string().min(5,"Address must be atleast 5 character").max(400,"Address cannot be more than 400 characters"),
+    Password:z.string().min(8,"password must be atleast 8 characters").max(16,"password cannot be more than 30 characters").regex(/[A-Z]/,"password must include an uppercase letter").regex(/[^A-Za-z0-9]/,"password must include a special character") 
+})
+
+export const loginShema = z.object({
+    Email:z.email().min(5,"email must be atleast 5 characters").max(30,"email cannot be more than 30 characters"),
+    Password:z.string().min(1,"password is required")
+})
