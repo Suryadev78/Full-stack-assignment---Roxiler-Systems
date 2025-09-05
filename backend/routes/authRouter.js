c4f56b83bbe4cc06b717c22f94acb65ca8fe9c82
suryadev78 <pandeysuryadev00@gmail.com>
2025-09-05 11:21:46 +0530

    added update router

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { login, signup } from "../controllers/authController.js";
+import { login, signup, updatePassword } from "../controllers/authController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 
 const authRouter = Router();
 
 authRouter.post("/signup",signup);
 authRouter.post("/login",login);
+authRouter.put("/update-password",authMiddleware,updatePassword);
 
 export default authRouter;
