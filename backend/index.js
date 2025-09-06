eb0fec516fd3506c1b61e7ebcb16b8d26bbecb63
suryadev78 <pandeysuryadev00@gmail.com>
2025-09-06 14:04:13 +0530

    final index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv"
+dotenv.config();
 import express from "express";
 import cors from "cors";
+
+
 import mainRouter from "./routes/mainRouter.js";
 // import { Router } from "express";
 // import 
@@ -8,17 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1",mainRouter);
-
-
-
-
-
-
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
