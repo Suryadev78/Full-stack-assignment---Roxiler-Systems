4e329905443c082fa6f0e292b38cbd1a240b7916
suryadev78 <pandeysuryadev00@gmail.com>
2025-09-07 05:02:47 +0530

    added routing to support different pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import LoginPage from './pages/login'
+import SignUpPage from './pages/signup'
+import { Routes,Route } from 'react-router-dom'
+import AdminDashboard from './pages/adminDashboard'
+import ProtectedRoute from './components/ProtectedRoute'
+import UnAuthorized from './pages/unAuthorized'
+import NormalUserDashboard from './pages/storeListings'
+import StoreOwnerDashboard from './pages/storeOwnerDashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className=' w-full h-screen font-bold flex justify-center justify-items-center'>
-        <h1 className='text-2xl'>Hello Vite!</h1>
-     </div>
-    </>
+    <Routes>
+      <Route path ="/login" element={<LoginPage/>}/>
+      <Route path="/signup" element={<SignUpPage/>}/>
+      <Route path="/admin/dashboard" element={<ProtectedRoute allowedRoles={["ADMIN"]}><AdminDashboard
+      /></ProtectedRoute>}/>
+      <Route path="/unauthorized" element={<UnAuthorized/>}/>
+      <Route path="/store-listings" element={<ProtectedRoute allowedRoles={["NORMAL_USER","ADMIN"]}><NormalUserDashboard/></ProtectedRoute>}/>
+      <Route
+       path="/store-owner-dashboard"
+       element={
+       <ProtectedRoute allowedRoles={["STORE_OWNER"]}>
+       <StoreOwnerDashboard />
+       </ProtectedRoute>
+        }
+/>
+    </Routes>
+    
   )
 }
 
