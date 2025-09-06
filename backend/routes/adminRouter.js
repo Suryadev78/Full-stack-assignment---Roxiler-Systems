54ef3e4f539cf877f47c1440ab04ce11369f5d65
suryadev78 <pandeysuryadev00@gmail.com>
2025-09-06 13:53:36 +0530

    completed adminRouter.js

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import { authMiddleware, isAdmin } from "../middleware/authMiddleware.js";
+import { createUserOrAdmin } from "../controllers/adminCreateUser_Admin.js";
+import { createStore } from "../controllers/storeCreation.js";
+import { adminDashboard } from "../controllers/adminDashboard.js";
+import { listStoresByFilter } from "../controllers/listStoresByFilter.js";
+import { listUsersByFilter } from "../controllers/listUsersByFilter.js";
 
 const adminRouter = Router();
 
-adminRouter.get("/getAdmin",(req,res)=>{
-    res.send("Hello from adminRouter");
-})
+adminRouter.post("/create-store",authMiddleware,isAdmin,createStore);
+adminRouter.post("/create-user",authMiddleware,isAdmin,createUserOrAdmin);
+adminRouter.get("/dashboard",authMiddleware,isAdmin,adminDashboard)
+adminRouter.get("/list-users",authMiddleware,isAdmin,listUsersByFilter);
+adminRouter.get("/list-stores",authMiddleware,isAdmin,listStoresByFilter)
 export default adminRouter;
